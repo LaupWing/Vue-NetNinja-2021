ab9f43494d419f3b654d74b622578c4cc9d5b156
LaupWing <loc_strict@hotmail.com>
2021-05-25 17:18:30 +0200

    72

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
    <div class="home">
-      Home
+      <h1>Home</h1>
+      <p ref="p">My name is {{name}} my age is {{age}}</p>
+      <button @click="handleClick">Click Me</button>
    </div>
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
    name: "Home",
    setup(){
-      
+      const p = ref(null)
+      const handleClick = ()=>{
+         console.log(p)
+      }
+
+      return {
+         p,
+         handleClick
+      }
    }
 };
 </script>

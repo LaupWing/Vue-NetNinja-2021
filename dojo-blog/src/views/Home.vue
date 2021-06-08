50c684582a10db83dd34677b044d4faa402ce517
LaupWing <loc_strict@hotmail.com>
2021-06-08 16:08:38 +0200

    heh

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,35 +1,35 @@
 <template>
-   <div class="home">
+   <!-- <div class="home">
       <h1>Home</h1>
       <p ref="p">My name is {{name}} my age is {{age}}</p>
       <button @click="handleClick">Click Me</button>
-   </div>
+   </div> -->
 </template>
 
 <script>
-import {computed, ref} from 'vue'
+// import {computed, ref} from 'vue'
 
 export default {
-   name: "Home",
-   setup(){
-      const p = ref(null)
-      const name = ref('mario')
-      const age = ref(30)
-      const name2 = computed(()=>{
-         return 'Shaun'
-      })
+   // name: "Home",
+   // setup(){
+   //    const p = ref(null)
+   //    const name = ref('mario')
+   //    const age = ref(30)
+   //    const name2 = computed(()=>{
+   //       return 'Shaun'
+   //    })
 
-      const handleClick = ()=>{
-         name.value = 'Luigi'
-         age.value = 30
-      }
+   //    const handleClick = ()=>{
+   //       name.value = 'Luigi'
+   //       age.value = 30
+   //    }
 
-      return {
-         p,
-         name,
-         age,
-         handleClick
-      }
-   }
+   //    return {
+   //       p,
+   //       name,
+   //       age,
+   //       handleClick
+   //    }
+   // }
 };
 </script>

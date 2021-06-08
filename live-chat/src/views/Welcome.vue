e3e3f656da2fd5052a9c7ad82d6e1136fa02fae1
LaupWing <loc_strict@hotmail.com>
2021-06-08 16:30:19 +0200

    103

--- live-chat/src/views/Welcome.vue
+++ live-chat/src/views/Welcome.vue
@@ -1,23 +1,44 @@
 <template>
    <div class="welcome container">
       <p>Welcome</p>
-      <SignupForm/>
+      <SignupForm />
+      <LoginForm />
    </div>
 </template>
 
 <script>
-import SignupForm from '../components/SignupForm.vue'
+import SignupForm from "../components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
 
 export default {
-   components:{
-      SignupForm
-   }
-}
+   components: {
+      SignupForm,
+      LoginForm,
+   },
+};
 </script>
 
 <style>
-   .welcome{
-      text-align: center;
-      padding: 20px 0;
-   }
+.welcome {
+   text-align: center;
+   padding: 20px 0;
+}
+/* form styles */
+.welcome form {
+   width: 300px;
+   margin: 20px auto;
+}
+.welcome label {
+   display: block;
+   margin: 20px 0 10px;
+}
+.welcome input {
+   width: 100%;
+   padding: 10px;
+   border-radius: 20px;
+   border: 1px solid #eee;
+   outline: none;
+   color: #999;
+   margin: 10px auto;
+}
 </style>

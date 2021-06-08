494c24db9049bc5ac26807a4bcc6e5c214673a17
LaupWing <loc_strict@hotmail.com>
2021-06-08 16:29:19 +0200

    103

---
+++ live-chat/src/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+   <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Login</button>
+   </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+   setup(){
+      const email = ref()
+      const password = ref()
+
+      const handleSubmit = ()=>{
+         console.log({
+            email: email.value,
+            password: password.value
+         })
+      }
+
+      return {
+         email,
+         password,
+         handleSubmit
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ live-chat/src/components/SignupForm.vue
@@ -0,0 +1,39 @@
+<template>
+   <form @submit.prevent="handleSubmit"> 
+      <input type="text" required placeholder="display name" v-model="displayName">
+      <input type="text" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Sign up</button>
+   </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+   setup(){
+      const displayName = ref()
+      const email = ref()
+      const password = ref()
+
+      const handleSubmit = ()=>{
+         console.log({
+            displayName: displayName.value,
+            email: email.value,
+            password: password.value
+         })
+      }
+
+      return {
+         displayName,
+         email,
+         password,
+         handleSubmit
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>

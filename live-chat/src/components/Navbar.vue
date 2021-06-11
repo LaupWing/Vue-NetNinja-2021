2c369d884bc5e521c692b2fd96bcd52bf32649e1
LaupWing <loc_strict@hotmail.com>
2021-06-11 14:33:31 +0200

    110

---
+++ live-chat/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+   <nav>
+      <div>
+         <p>Hey there ... display name here</p>
+         <p class="email">Currently loggin in as ... email</p>
+      </div>
+      <button>Logout</button>
+   </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
